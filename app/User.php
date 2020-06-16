779d318373258973ebec4cb63de5f4fa0abc5e71
moinur-najib <moinurnajib@gmail.com>
2020-06-16 08:44:14 +0600

    Fixed bugs, working on email verification

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'username', 'phone_no', 'district_id', 'division_id', 'street_adress', 'email', 'password', 'ip_adress', 'remember_token',
     ];
 
     /**
